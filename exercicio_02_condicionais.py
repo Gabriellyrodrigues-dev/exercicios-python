6eeb0a9d38b359b78aee3fb79f61faf5d59a3ed3
Gabrielly Rodrigues <mariagabriellybrodrigues@gmail.com>
2026-01-20 13:11:36 -0300

    Adiciona exercício 02 de condicionais

---
+++ exercicio_02_condicionais.py
@@ -0,0 +1,21 @@
+# Exercício 02 - Condicionais
+# Verificar idade e nota de um aluno
+
+idade = 17
+
+if idade >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+
+
+nota = 8
+
+if nota >= 9:
+    print("Excelente")
+elif nota >= 7:
+    print("Bom")
+elif nota >= 5:
+    print("Recuperação")
+else:
+    print("Reprovado")
